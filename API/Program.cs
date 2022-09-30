e76bc012b25f4f99cbd4eccba1ea0f6e7d9bf92f
rtaf <rtafuni20@yahoo.com>
2022-09-30 15:25:20 +0300

    End of section 12

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,13 @@
 using System;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using Persistence;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace API
 {
@@ -19,16 +21,17 @@ namespace API
 
             var services = scope.ServiceProvider;
 
-            try
+            try 
             {
                 var context = services.GetRequiredService<DataContext>();
+                var userManager = services.GetRequiredService<UserManager<AppUser>>();
                 await context.Database.MigrateAsync();
-                await Seed.SeedData(context);
+                await Seed.SeedData(context, userManager);
             }
             catch (Exception ex)
             {
                 var logger = services.GetRequiredService<ILogger<Program>>();
-                logger.LogError(ex, "An error occured during migration");
+                logger.LogError(ex, "An error occured during migraiton");
             }
 
             await host.RunAsync();
